12ef28c44ff003a6abfb4c2bfd8a3bcfcce20993
Mershelle Rivera <mershellemae@gmail.com>
2024-12-30 15:08:50 -0800

    skeleton MazeUICompleted

---
+++ src/gui/MazeUICompleted.java
@@ -0,0 +1,31 @@
+package gui;
+
+import generation.AlgorithmType;
+
+/**
+ * @author Mershelle Rivera
+ * @version 1.0
+ *
+ * Class that generates and solves the maze
+ */
+public class MazeUICompleted extends MazeUI
+{
+
+    @Override
+    public void runAlgorithm(AlgorithmType type)
+    {
+
+    }
+
+    /**
+     * Returns walls north, east, south, west
+     *
+     * @param vertex int vertex to get walls for
+     * @return bool[] array of walls
+     */
+    private boolean[] getWalls(int vertex)
+    {
+
+        return new boolean[0];
+    }
+}
